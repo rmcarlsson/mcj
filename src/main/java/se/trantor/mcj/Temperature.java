89a7a9312fa9f54e29025c75c2940b8bd3814162
Mikael Carlsson <rmcarlsson@gmail.com>
2016-07-05 00:18:53 +0200

    * Added com.pi4j, core and devices * Added repository for version 1.1-SNAPSHOT for pi4j. Needed for DS1820 support

--- src/main/java/se/trantor/mcj/Temperature.java
+++ src/main/java/se/trantor/mcj/Temperature.java
@@ -1,7 +1,13 @@
 package se.trantor.mcj;
 
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import com.pi4j.component.temperature.TemperatureSensor;
+import com.pi4j.component.temperature.impl.TmpDS18B20DeviceType;
+import com.pi4j.io.w1.W1Device;
+import com.pi4j.io.w1.W1Master;
 
 public class Temperature {
 	
@@ -10,7 +16,7 @@ public class Temperature {
 	private Heater heater;
 	private PidController pidController;
 	private static final Logger logger = Logger.getLogger(McNgMain.class.getName());
-	
+	private W1Device device = null;
 	
 	public Temperature(String aId)
 	{
@@ -20,7 +26,21 @@ public class Temperature {
 
 	public double GetTemperature()
 	{
-		int val = heater.getPower();
+		
+		if (device == null)
+		{
+			W1Master master = new W1Master();
+			List<W1Device> w1Devices = master.getDevices(TmpDS18B20DeviceType.FAMILY_CODE);
+			for (W1Device device : w1Devices) {
+				logger.log(Level.INFO, "Found a 1-Wire temperature sensor, ID: {0}", device.getId());	            //returns the ID of the Sensor and the  full text of the virtual file
+				this.device = device; 
+			}
+		}
+		
+
+        logger.log(Level.INFO, "Temperature is {0}", ((TemperatureSensor) device).getTemperature());
+		
+        int val = heater.getPower();
 		double diff = (val/2) - temperature;
 		
 		temperature += diff/50;
